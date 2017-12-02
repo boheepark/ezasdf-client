6c5350f9b742cefe3c08ce86864e2b045cb33032
Bohee Park <boheepark91@gmail.com>
2017-12-02 03:00:32 -0500

    react-bootstrap and forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,40 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import axios from 'axios'
+import UsersList from './components/UsersList'
+import AddUser from './components/AddUser'
+import About from './components/About'
+import NavBar from './components/NavBar'
+import Form from './components/Form'
 
-import UsersList from './components/UsersList';
-import AddUser from './components/AddUser';
-import About from './components/About';
 
 class App extends Component {
+
     constructor() {
         super();
         this.state = {
             users: [],
             username: '',
-            email: ''
-        }
+            email: '',
+            title: 'ezasdf',
+            formData: {
+                username: '',
+                email: '',
+                password: ''
+            }
+        };
     }
+
     componentDidMount() {
         this.getUsers();
     }
+
     getUsers() {
         axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
             .then((res) => { this.setState({ users: res.data.data.users }); })
             .catch((err) => { console.log(err); })
     }
+
     addUser(event) {
         event.preventDefault();
         const data = {
@@ -36,34 +48,53 @@ class App extends Component {
             })
             .catch((err) => { console.log(err); })
     }
+
     handleChange(event) {
         const obj = {};
         obj[event.target.name] = event.target.value;
         this.setState(obj);
     }
+
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">
-                        <br/>
-                        <Switch>
-                            <Route exact path='/' render={() => (
-                                <div>
-                                    <h1>All Users</h1>
-                                    <hr/><br/>
-                                    <AddUser
-                                        username={this.state.username}
-                                        email={this.state.email}
-                                        handleChange={this.handleChange.bind(this)}
-                                        addUser={this.addUser.bind(this)}
+            <div>
+                <NavBar
+                    title={this.state.title}
+                />
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6">
+                            <br/>
+                            <Switch>
+                                <Route exact path='/' render={() => (
+                                    <div>
+                                        <h1>All Users</h1>
+                                        <hr/><br/>
+                                        <AddUser
+                                            username={this.state.username}
+                                            email={this.state.email}
+                                            handleChange={this.handleChange.bind(this)}
+                                            addUser={this.addUser.bind(this)}
+                                        />
+                                        <br/>
+                                        <UsersList users={this.state.users}/>
+                                    </div>
+                                )} />
+                                <Route exact path='/register' render={() => (
+                                    <Form
+                                        formType={'Register'}
+                                        formData={this.state.formData}
                                     />
-                                    <br/>
-                                    <UsersList users={this.state.users}/>
-                                </div>
-                            )} />
-                            <Route exact path='/about' component={About}/>
-                        </Switch>
+                                )} />
+                                <Route exact path='/login' render={() => (
+                                    <Form
+                                        formType={'Login'}
+                                        formData={this.state.formData}
+                                    />
+                                )} />
+                                <Route exact path='/about' component={About}/>
+                            </Switch>
+                        </div>
                     </div>
                 </div>
             </div>
